55dd30fb96b434d2e4fdf9966a2b41cb3d5ed671
tranthien3004 <tus.sirovai@gmail.com>
2017-12-23 20:58:46 +0700

    Controller and View

---
+++ DoAnTA/Controllers/CHUDEController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WEBSITEBANSACH.Models;
+namespace WEBSITEBANSACH.Controllers
+{
+    public class CHUDEController : Controller
+    {
+        //
+        // GET: /CHUDE/
+     
+        //bam vo xem gio hang de xem gio 
+        SACHEntities db = new SACHEntities();
+        public PartialViewResult CHUDEpartial()
+        {
+            var theloai = db.CHUDEs.ToList();
+            return PartialView(theloai);
+        }
+        public ActionResult SPTHEOCHUDE(int id)
+        {
+            var sach = from s in db.CHITIETSACHes where  s.MACHUDE == id select s;
+            return View(sach);
+        }
+	}
+}
