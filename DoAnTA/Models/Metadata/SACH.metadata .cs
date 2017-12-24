ce95a2c670177787b8f0c51927c06e8530a3a557
nhokshin32 <shinvan240994@gmail.com>
2017-12-24 22:49:43 +0700

    Add them Models Metadata

---
+++ DoAnTA/Models/Metadata/SACH.metadata .cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WEBSITEBANSACH.Models.Metadata
+{
+    [MetadataTypeAttribute(typeof(SACHMetadata))]
+    public partial class SACH
+    {
+        internal sealed class SACHMetadata
+        {
+            [Display(Name = "Mã Sách")]
+            //[Required(ErrorMessage = "Vui Lòng Nhập Dữ Liệu Cho Trường Này.")]
+            public int MaSach { get; set; }
+            [Display(Name = "Tên Sách")]
+            //[Required(ErrorMessage = "Vui Lòng Nhập Dữ Liệu Cho Trường Này.")]
+            public string Tensach { get; set; }
+         
+
+
+        }
+    }
+}
