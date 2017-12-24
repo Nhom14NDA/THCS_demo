6cbd0bf70688f521f1fdb394d46c01a373003515
TonyNhat <thaidangnhat1996@gmail.com>
2017-12-24 23:06:20 +0700

    Add them GioHangPartial vao models

---
+++ DoAnTA/Controllers/GioHangController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WEBSITEBANSACH.Models;
+namespace WEBSITEBANSACH.Controllers
+{
+    public class GioHangController : Controller
+    {
+        
+        SACHEntities db = new SACHEntities();
+        public List<GioHangModels> LayGioHang()
+        {
+            List<GioHangModels> lstGioHang = Session["GioHang"] as List<GioHangModels>;
+            if (lstGioHang == null)
+            {
+                lstGioHang = new List<GioHangModels>();
+                Session["GioHang"] = lstGioHang;
+            }
+            return lstGioHang;
+        }
+        
+}
---
+++ DoAnTA/Models/GioHangModels.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEBSITEBANSACH.Models
+{
+    public class GioHangModels
+    {
+        SACHEntities db = new SACHEntities();
+        public List<CHITIETSACH> Items = new List<CHITIETSACH>();
+        public int iMaSach { set; get; }
+        public string sTenSach { set; get; }
+        public string sAnhBia { set; get; }
+        public double dDongia { set; get; }
+        public int iSoLuong { set; get; }
+        public double dThanhTien
+        {
+            get { return iSoLuong * dDongia; }
+        }
+        public int soluong
+        {
+            get
+            {
+                var sl = Items.Sum(p =>p.Soluongton);
+                return soluong;
+            }
+        }
+
+  
+        public GioHangModels(int MaSach)
+        {
+            iMaSach = MaSach;
+            CHITIETSACH ctsanh = db.CHITIETSACHes.Single(n => n.MaCTS==iMaSach);
+            sTenSach = ctsanh.TensachCT;
+            sAnhBia = ctsanh.Anhbia;
+            dDongia = double.Parse(ctsanh.Dongia.ToString());
+            iSoLuong = 1;
+
+        }
+
+        
+
+       
+    }
+}
